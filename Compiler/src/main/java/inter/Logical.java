85b10f558945f2f991dfb38a93b03c1aad78b0be
guangxush <guangxush@163.com>
2020-07-04 14:41:14 +0800

    logical

---
+++ Compiler/src/main/java/inter/Logical.java
@@ -0,0 +1,30 @@
+package inter;
+
+import lexer.Token;
+import symbols.Type;
+
+/**
+ * @author: guangxush
+ * @create: 2020/06/30
+ */
+public class Logical extends Expr {
+    public Expr expr1, expr2;
+
+    Logical(Token token, Expr expr1, Expr expr2) {
+        super(token, null);
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+        type = check(expr1.type, expr2.type);
+        if (type == null) {
+            error("type error");
+        }
+    }
+
+    public Type check(Type p1, Type p2) {
+        if (p1 == Type.Bool && p2 == Type.Bool) {
+            return Type.Bool;
+        }
+        return null;
+    }
+
+}
