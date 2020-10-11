41ceab4db173260f38c83c64f9e3a4cb553ef6eb
guangxush <guangxush@163.com>
2020-10-11 13:12:11 +0800

    file

--- HappyParse/src/main/java/ChannelFileReaderAndWriter.java
+++ HappyParse/src/main/java/ChannelFileReaderAndWriter.java
@@ -1,107 +1,21 @@
-### 各种java解析小工具
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
-#### 将变量转换成静态常量值或枚举值
-
-输入：
-```text
-helloWorldProperty
-```
-
-输出：
-```text
-HELLO_WORLD_PROPERTY
-```
-
-[方法](./src/main/java/ParseVariable2Constant.java):
-```java
-    /**
-     * 变量转化成常量
-     * @param param
-     * @return
-     */
-    private static String variable2Constant(String param){
-        char[] array = param.toCharArray();
-        String result = "";
-        for(int i=0;i<array.length;i++){
-            result += array[i];
-            // 防止数组越界
-            if(i+1==array.length){
-                result = result.toUpperCase();
-                break;
-            }
-            // 遇到下一个为大写字母前面要加一个_
-            if('A'<=array[i+1] && array[i+1]<='Z'){
-                result += "_";
-                result = result.toUpperCase();
-            }
-        }
-        return result;
-    }
-```
-
-#### 将数据库字段解析为变量
-
-输入：
-```text
-userId,用户ID
-```
-
-输出：
-```text
 /**
- * 用户ID
+ * Java使用Channel读取和写入大文件
+ *
+ * @author: guangxush
+ * @create: 2020/10/11
  */
-private String userId;
-```
+public class ChannelFileReaderAndWriter {
+    private RandomAccessFile inFile = null;
+    private RandomAccessFile outFile = null;
+    private FileChannel inChannel = null;
+    private FileChannel outChannel = null;
+    private final ByteBuffer buf = ByteBuffer.allocate(1024);
 
-[方法](./src/main/java/ParseDB2Property.java):
-```java
-    /**
-     * 解析数据库字段
-     * @param inputDbColumn 输入格式为 userId,用户ID
-     * @return
-     */
-    private static String parse(String inputDbColumn){
-        String[] array = inputDbColumn.split(",");
-        return parseNote(array[1]) + "\n" + parseProperty(array[0]);
-    }
-
-    /**
-     * 生成注释
-     * @param comment
-     * @return
-     */
-    private static String parseNote(String comment){
-        return String.format("/**\n" +
-                " * %s\n" +
-                " */", comment);
-    }
-
-    /**
-     * 生成变量，只针对varchar --> String
-     * @param value
-     * @return
-     */
-    private static String parseProperty(String value){
-        char[] array = value.toCharArray();
-        StringBuffer buffer = new StringBuffer();
-        for(int i=0;i<array.length;i++){
-            if(array[i]=='_'){
-                // 下一位转换成大写字母
-                array[i+1] -= 32;
-                continue;
-            }
-            buffer.append(array[i]);
-        }
-        String property = String.format("private String %s;", buffer.toString());
-        return property;
-    }
-```
-
-#### Java使用FileChannel实现大文件读取、写入、复制、倒序复制
-
-[方法](./src/main/java/ChannelFileReaderAndWriter.java):
-```java
     /**
      * 写文件
      * @param fileName
@@ -215,8 +129,12 @@ private String userId;
             outChannel.write(buf);
         }
     }
-```
-文件按照行倒序输出可以使用这个命名
-```shell script
-perl -e 'print reverse <>' filename
-```
+
+    public static void main(String[] args) throws IOException {
+        ChannelFileReaderAndWriter tool = new ChannelFileReaderAndWriter();
+        //tool.doRead("HappyParse/src/main/resources/SourceFile.txt");
+        //tool.doWrite("HappyParse/src/main/resources/TargetFile.txt");
+        //tool.doCopy("HappyParse/src/main/resources/SourceFile.txt", "HappyParse/src/main/resources/CopyFile.txt");
+        tool.reverseCopy("HappyParse/src/main/resources/SourceFile.txt", "HappyParse/src/main/resources/ReverseCopyFile.txt");
+    }
+}
